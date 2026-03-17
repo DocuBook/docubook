1d53b232c7c35d0e1b6110cdc02284dd0ed73ead
gitfromwildan <201965665+gitfromwildan@users.noreply.github.com>
2026-03-18 01:45:15 +0700

    feat: add environment session script for Git author configuration

---
+++ env-session.sh
@@ -0,0 +1,4 @@
+export GIT_AUTHOR_NAME="gitfromwildan"
+export GIT_AUTHOR_EMAIL="wnn.indomaret@gmail.com"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
