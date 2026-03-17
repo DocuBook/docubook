0fadb2334404c6b7a99a3bc472e39e1407992804
gitfromwildan <201965665+gitfromwildan@users.noreply.github.com>
2026-03-18 02:12:16 +0700

    delete: remove environment session script for Git author configuration

--- env-session.sh
+++
@@ -1,4 +0,0 @@
-export GIT_AUTHOR_NAME="gitfromwildan"
-export GIT_AUTHOR_EMAIL="wnn.indomaret@gmail.com"
-export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
