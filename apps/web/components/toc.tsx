b1647bc635f8a42eaa531a3d6f7877b32fac9135
gitfromwildan <201965665+gitfromwildan@users.noreply.github.com>
2026-03-19 16:48:35 +0700

    fix: adjust minimum width of Toc component for consistency and reposition Sponsor component

--- apps/web/components/toc.tsx
+++ apps/web/components/toc.tsx
@@ -11,7 +11,7 @@ export default function Toc({ tocs }: { tocs: TocItem[] }) {
   const { activeId, setActiveId } = useActiveSection(tocs)
 
   return (
-    <div className="toc flex-3 sticky top-4 hidden h-[calc(100vh-8rem)] min-w-[238px] self-start lg:flex lg:p-8">
+    <div className="toc flex-3 sticky top-4 hidden h-[calc(100vh-8rem)] min-w-[240px] self-start lg:flex lg:p-8">
       <div className="flex h-full w-full flex-col gap-2 px-2">
         <div className="flex items-center gap-2">
           <ListIcon className="h-4 w-4" />
@@ -20,10 +20,10 @@ export default function Toc({ tocs }: { tocs: TocItem[] }) {
         <div className="flex-1 min-h-0">
           <ScrollArea className="h-full">
             <TocObserver data={tocs} activeId={activeId} onActiveIdChange={setActiveId} />
+            <div className="mt-4">
+              <Sponsor />
+            </div>
           </ScrollArea>
-        </div>
-        <div className="mt-auto">
-          <Sponsor />
         </div>
       </div>
     </div>
