1ed77f56e6aa8cd9095c62045be0f8cbc448a4dc
gitfromwildan <201965665+gitfromwildan@users.noreply.github.com>
2026-05-09 12:25:03 +0700

    feat: components base collapse and drawer

--- packages/template/native/tailwind.config.ts
+++ packages/template/native/tailwind.config.ts
@@ -1,16 +1,8 @@
 import type { Config } from "tailwindcss";
-import daisyui from "daisyui";
 
 export default {
   content: [
     "./.docu/components/**/*.{js,ts,jsx,tsx}",
     "./docs/**/*.mdx",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
 } satisfies Config;
