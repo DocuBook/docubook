983941588996bd5b967546e5455d2eb4f832f343
gitfromwildan <201965665+gitfromwildan@users.noreply.github.com>
2026-05-10 18:58:49 +0700

    chore: commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,42 +1,52 @@
 /** @type {import('czg').GlobalOptions} */
 const config = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
       ],
     ],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'header-max-length': [2, 'always', 100],
+    "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
+    "header-max-length": [2, "always", 100],
   },
   prompt: {
     alias: {
-      fd: 'docs: fix typos',
-      af: 'fix: fix something',
+      fd: "docs: fix typos",
+      af: "fix: fix something",
     },
     customScopesAlign: {
-      when: 'always',
-      value: ['docs', 'app', 'packages', 'components', 'utils', 'hooks', 'types', 'configs', 'scripts'],
+      when: "always",
+      value: [
+        "docs",
+        "app",
+        "packages",
+        "components",
+        "utils",
+        "hooks",
+        "types",
+        "configs",
+        "scripts",
+      ],
     },
-    defaultScopes: ['app', 'packages'],
+    defaultScopes: ["app", "packages"],
     allowEmptyScopes: true,
     customScopesAlign: true,
-    emptyScopesAlias: 'empty',
+    emptyScopesAlias: "empty",
     upperCaseSubject: false,
   },
-}
+};
 
-export default config
+export default config;
